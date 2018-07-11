2e3f0f0389b677242c9db10e3600e2f163bc53bf
Harris Beg <legostarwarsguy02@gmail.com>
2018-07-11 09:46:07 -0700

    added JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+var hyperchargedApp = angular.module("mainPage");
+app.directive("header"), function() {
+	return {
+		template: "<nav class="white navbar" role="navigation"> <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo"><img src="/assets/headlogo.png" class="navimg"/></a> <ul class="right hide-on-med-and-down"> <li><a href="#" class="light-blue-text lighten-1">About</a></li> <li><a href="#" class="light-blue-text lighten-1">Contact</a></li> <li><a href="#" class="light-blue-text lighten-1">Images</a></li> <li><a href="#" class="light-blue-text lighten-1">Calendar</a></li> <li><a href="https://www.youtube.com/channel/UCWX3ATF1LEWSbaPkAaDgxZw" class="red-text lighten-1"><i class="fa fa-youtube small"></i></li> </ul><ul id="nav-mobile" class="side-nav"> <li><a href="#" class="light-blue-text lighten-1">About</a></li> <li><a href="#" class="light-blue-text lighten-1">Contact</a></li> <li><a href="#" class="light-blue-text lighten-1">Images</a></li> <li><a href="#" class="light-blue-text lighten-1">Calendar</a></li> <li><a href="https://www.youtube.com/channel/UCWX3ATF1LEWSbaPkAaDgxZw" class="red-text lighten-1">YouTube</li> </ul> <a href="#" data-activates="nav-mobile" class="button-collapse black-text"><i class="material-icons">menu</i></a> </div> </nav> <div class="section no-pad-bot" id="index-banner"> <div class="parallax-container"> <div class="parallax"> <img src="/assets/wall1.JPG" style="display: block; transform: translate3d(-50%, 20%, 0px);"/> </div> <h1 class="headpub white-text center" >Hypercharged</h1> </div></div>"
+	};
+};
